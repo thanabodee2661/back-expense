11b9da7ce91fff270ae747e219a8a27dcb8af101
thanabodee <thanabodee2661@gmail.com>
2020-11-14 00:26:34 +0700

    - Update DateUtil - Add function convertMilliToTimeLeftString

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var dateUtil = require('../src/util/dateUtil');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/testConvertDate', function(req, res, next) {
+  res.json(dateUtil.convertMilliToTimeLeftString(90005555));
 });
 
 module.exports = router;
